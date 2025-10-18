5f6a547b72f0adbba09e5be7608c11bf2c013eb5
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-18 23:53:33 +0600

    update code

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import { uptime } from 'process';
+import { timeStamp } from 'console';
+import config from './config';
 
 const app: Application = express();
 app.use(cors({
@@ -15,9 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+    
 });
 
 
