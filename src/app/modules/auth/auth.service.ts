e5b0865bd058b064a05e7e4c57adaa9b5271cc31
Mahdi Hassan <mahdioffi333@gmail.com>
2025-11-02 22:53:01 +0600

    Add jsonwebtoken dependency and implement JWT token generation in AuthService

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { UserStatus } from "@prisma/client";
 import { prisma } from "../../shared/prisma";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const login = async(payload: { email: string; password: string }) => {
   const user = await prisma.user.findUniqueOrThrow({
@@ -13,8 +14,21 @@ const login = async(payload: { email: string; password: string }) => {
   const isCorrectPassword = await bcrypt.compare(payload.password, user.password);
   if(!isCorrectPassword){
     throw new Error('Password is incorrect');
+  }
 
-    
+  const accessToken = jwt.sign({email: user.email, role: user.role}, "abcd", {
+    algorithm: "HS256",
+    expiresIn: "1h"
+  })
+
+  const refreshToken = jwt.sign({email: user.email, role: user.role}, "abcd", {
+    algorithm: "HS256",
+    expiresIn: "90d"
+  })
+
+  return {
+    accessToken,
+    refreshToken
   }
 }
 
