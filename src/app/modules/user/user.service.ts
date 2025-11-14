252f2588bf877e4d0fba3b3ea020c48c8cb5f012
Mahdi Hassan <mahdioffi333@gmail.com>
2025-11-14 18:36:07 +0600

    Implement createDoctor function in UserService to handle doctor creation and profile photo upload

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -61,6 +61,38 @@ const createAdmin = async(req: Request): Promise<Admin> => {
   return result;
 }
 
+
+const createDoctor = async (req: Request): Promise<Doctor> => {
+
+    const file = req.file;
+
+    if (file) {
+        const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+        req.body.doctor.profilePhoto = uploadToCloudinary?.secure_url
+    }
+    const hashedPassword: string = await bcrypt.hash(req.body.password, 10)
+
+    const userData = {
+        email: req.body.doctor.email,
+        password: hashedPassword,
+        role: UserRole.DOCTOR
+    }
+
+    const result = await prisma.$transaction(async (transactionClient) => {
+        await transactionClient.user.create({
+            data: userData
+        });
+
+        const createdDoctorData = await transactionClient.doctor.create({
+            data: req.body.doctor
+        });
+
+        return createdDoctorData;
+    });
+
+    return result;
+};
+
 const allUserFromDB = async (params: any, options: any) => {
   const {page, limit, skip, sortBy, sortOrder} = PaginationHelper.calculatePagination(options);
   const {searchTerm, ...filterData} = params;
@@ -119,6 +151,7 @@ const allUserFromDB = async (params: any, options: any) => {
 
 export const UserService = {
   createPatient,
-  allUserFromDB,
-  createAdmin
+  createAdmin,
+  createDoctor,
+  allUserFromDB
 }
