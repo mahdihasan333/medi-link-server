faff7db2ff5940455e3e8ebac73b743dfe7067e1
Mahdi Hassan <mahdioffi333@gmail.com>
2025-11-14 18:18:24 +0600

    Refactor user routes: remove commented code and ensure create-doctor route is properly defined

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -17,8 +17,6 @@ router.post(
         req.body = UserValidation.createPatientValidationSchema.parse(JSON.parse(req.body.data))
         return UserController.createPatient(req, res, next)
     }
-    // create doctor
-    // create admin
 
 )
 
@@ -29,6 +27,18 @@ router.post(
     (req: Request, res: Response, next: NextFunction) => {
         req.body = UserValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
         return UserController.createAdmin(req, res, next)
+    }
+);
+
+
+router.post(
+    "/create-doctor",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log(JSON.parse(req.body.data))
+        req.body = UserValidation.createDoctorValidationSchema.parse(JSON.parse(req.body.data))
+        return UserController.createDoctor(req, res, next)
     }
 );
 
